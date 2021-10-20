b04683480c465fb38ef03289a06d2bdf2bb14d08
Andrea Carratta <46504271+devandreacarratta@users.noreply.github.com>
2021-10-20 22:12:56 +0200

    Hello World Nuget - Source Code (#1)

---
+++ source/DevAndreaCarratta.HelloWorld/SayHelloWorld.cs
@@ -0,0 +1,22 @@
+﻿namespace DevAndreaCarratta.HelloWorld
+{
+    public class SayHelloWorld
+    {
+        private readonly SayHelloWorldLogic _logic;
+
+        public SayHelloWorld()
+        {
+            _logic = new SayHelloWorldLogic();
+        }
+
+        public string SayToAnonymous()
+        {
+            return _logic.SayToAnonymous();
+        }
+
+        public string SayToFriend(string friend)
+        {
+            return _logic.SayToFriend(friend);
+        }
+    }
+}
